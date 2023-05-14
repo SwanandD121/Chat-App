350881e9376f1a54c2cbba06b59fd36f78a8055e
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-05-14 21:48:02 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID ="9e063f02-10f9-40b5-a0f6-c2f3963ca857"
+            userName="gigachad69"
+            userSecret="gigachad69"
+            />
+    );
+}
+
+export default App;
